2a3d562877977371ee626de80a5fad37dd7cf7f7
mominimtiaz <momin.imtiaz@rolustech.net>
2022-08-04 16:48:50 +0500

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,11 +16,15 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
