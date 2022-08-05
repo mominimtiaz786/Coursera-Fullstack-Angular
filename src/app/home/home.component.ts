5529cd03388572d58ee9a42ec95ae3050e371439
mominimtiaz <momin.imtiaz@rolustech.net>
2022-08-05 12:30:30 +0500

    Assignment2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DishService } from '../services/dish.service';
+import { LeaderService } from '../services/leader.service';
 import { PromotionService } from '../services/promotion.service';
 import { Dish } from '../shared/dish';
+import { Leader } from '../shared/leader';
 import { Promotion } from '../shared/promotion';
 
 @Component({
@@ -13,13 +15,16 @@ export class HomeComponent implements OnInit {
 
   promotion: Promotion;
   dish: Dish;
+  leader: Leader;
 
   constructor(private promotionService: PromotionService,
-  private dishService: DishService) { }
+  private dishService: DishService,
+  private leaderService: LeaderService) { }
 
   ngOnInit() {
     this.promotion = this.promotionService.getFeaturedPromotion();
     this.dish = this.dishService.getFeaturedDish();
+    this.leader = this.leaderService.getFeaturedLeader();
   }
 
 }
