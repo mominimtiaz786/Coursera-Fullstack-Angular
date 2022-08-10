a949a362edad9996ab4fad6b6991ae97dfbd612f
mominimtiaz <momin.imtiaz@rolustech.net>
2022-08-10 11:14:27 +0500

    Promise Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,8 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -64,7 +66,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatDialogModule, 
     MatSelectModule, 
     MatSlideToggleModule, 
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
   ],
   providers: [DishService,
     PromotionService,
