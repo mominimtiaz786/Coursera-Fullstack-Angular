318e3b4bf153c24044eaff44662246a012fa5909
mominimtiaz786 <21100179@lums.edu.pk>
2022-08-13 17:40:31 +0500

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -9,13 +9,32 @@ import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
 
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* <=> *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
+
+  visibility = 'shown';
+
 
   @Input() dish: Dish;
   dishIds: string[];
@@ -125,7 +144,7 @@ export class DishdetailComponent implements OnInit {
     this.route.params.pipe(
       switchMap(
         (params: Params) => {
-          this.dish = null;
+          this.visibility='hidden';
           return this.dishService.getDish(params['id'])
         }
       ))
@@ -133,6 +152,7 @@ export class DishdetailComponent implements OnInit {
         this.dish = dish;
         this.dishcopy=dish;
         this.setPrevNext(dish.id);
+        this.visibility='shown'
       });
   }
 
